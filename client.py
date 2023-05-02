7f638997ef704e4a3c5591545efc5cc35e28a9b6
Carlos Zatarain <Niarataz01@gmail.com>
2023-05-02 13:43:54 -0500

    Spaced out print statements

--- client.py
+++ client.py
@@ -3,17 +3,21 @@ import socket
 if __name__ == "__main__":
 	HOST = input("Server IP: ")
 	PORT = int(input("Server Port: "))
+	print("Type 'quit' to exit.")
+
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
 
 		while (True):
-			query = input("What are you looking for? ")
+			query = input("\nWhat are you looking for? ")
 
-			if (query == 'q'):
+			if (query == 'quit'):
 				break
 
 			s.sendall(bytes(query, 'utf-8'))
 
 			reply = s.recv(1024)
-			print(reply.decode('utf-8'))
+			print('\n' + reply.decode('utf-8'))
+
+	print("\nGoodbye!")
